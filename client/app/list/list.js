1a39a61066d8f903e2df4c3be9484131326c26e1
Alice Yu <yaliceme@gmail.com>
2016-04-30 18:56:27 -0700

    Link home and list views, display listName
    modified:   client/app/home/home.html
    modified:   client/app/list/list.html
    modified:   client/app/list/list.js
    modified:   client/index.html

--- client/app/list/list.js
+++ client/app/list/list.js
@@ -2,11 +2,11 @@
 angular.module('list', [])
 .controller('ListController', function ($scope, $routeParams, DataFactory) {
   var lists = DataFactory.lists;
-  var listName = $routeParams.listName;
-
+  $scope.listName = $routeParams.listName;
   $scope.items = [];
+
   for (var i = 0; i < lists.length; i++) {
-    if (lists[i].listName === listName) {
+    if (lists[i].listName === $scope.listName) {
       $scope.items = lists[i].items;
     }
   }
