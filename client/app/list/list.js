5d8ce22cfdf2162150e5a415763579e3f5144d75
Alice Yu <yaliceme@gmail.com>
2016-04-30 18:47:05 -0700

    Display all todo items within each list view
    modified:   client/app/list/list.html
    modified:   client/app/list/list.js

--- client/app/list/list.js
+++ client/app/list/list.js
@@ -1,5 +1,13 @@
 "use strict";
 angular.module('list', [])
 .controller('ListController', function ($scope, $routeParams, DataFactory) {
-  $scope.test = "test";
+  var lists = DataFactory.lists;
+  var listName = $routeParams.listName;
+
+  $scope.items = [];
+  for (var i = 0; i < lists.length; i++) {
+    if (lists[i].listName === listName) {
+      $scope.items = lists[i].items;
+    }
+  }
 });
