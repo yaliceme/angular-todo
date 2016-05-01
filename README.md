5b038166f302d9538afd6b343acc86b0b71b4b54
Alice Yu <yaliceme@gmail.com>
2016-04-30 17:53:56 -0700

    Add instructions for running app with http-server modified:   README.md modified:   client/app/app.js

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Angular Todo
 
 Basic todo app with Angular
+
+To run this app:
+- clone down
+- install Node, if you don't have node
+- run `npm install http-server -g`, if you don't have http-server
+- run `http-server <file path to project directory>`
+- in your browser, go to localhost:8080/client
--- client/app/app.js
+++ client/app/app.js
@@ -6,11 +6,11 @@ angular.module('AngularTodo', [
 ])
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/', {
-    templateUrl: '/app/home/home.html',
+    templateUrl: '/client/app/home/home.html',
     controller: 'HomeController'
   });
   $routeProvider.when('/:listName', {
-    templateUrl: '/app/list/list.html',
+    templateUrl: '/client/app/list/list.html',
     controller: 'ListController'
   });
 }]);
